1ebf734197e7b86a487404e7342f392a93b34418
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2023-06-16 15:17:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BlockchainProvider } from './context/AppConfig';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { UploadNFT } from './pages/UploadNFT';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BlockchainProvider>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/upload" element={<UploadNFT/>} />
+      </Routes>
+      </BrowserRouter>
+
+      </BlockchainProvider>
     </div>
   );
 }
--- src/context/AppConfig.js
+++ src/context/AppConfig.js
@@ -1,58 +1,51 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Web3Modal from 'web3modal';
-import  * as eth  from 'ethers';
-import contr from '../Contracts/artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
-import axios from 'axios';
-
-import { NFTStorage, Blob } from 'nft.storage'
-import { FirebaseConfig } from './FirebaseConfig';
-const NFT_STORAGE_TOKEN = process.env.REACT_APP_PUBLIC_NFT_STORAGE_TOKEN
-const client = new NFTStorage({ token: NFT_STORAGE_TOKEN });
-const MarketAddress = process.env.REACT_APP_NFTMARKETPLACE_CONTRACT ;
-const MarketAddressABI=contr.abi;
-const fetchContract = (signerOrProvider) => new eth.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
+import React, { useContext, useEffect, useState } from "react";
+import Web3Modal from "web3modal";
+import * as eth from "ethers";
+import { NFTStorage, Blob } from "nft.storage";
+import { Contract } from "ethers";
+import abi from "./utils";
 
 export const BlockchainConfig = React.createContext();
 
 export const BlockchainProvider = ({ children }) => {
-  const nftCurrency = 'MATIC';
-  const [currentAccount, setCurrentAccount] = useState('');
-  // const {uploadArtOffChain} = useContext(FirebaseConfig)
+  
+  const contr_addr = process.env.REACT_APP_CONTRACT;
+  const NFT_STORAGE_TOKEN = process.env.REACT_APP_PUBLIC_NFT_STORAGE_TOKEN;
+  const client = new NFTStorage({ token: NFT_STORAGE_TOKEN });
+
+  const provider = new eth.JsonRpcProvider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new Contract(contr_addr, abi, signer);
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
   const connectWallet = async () => {
-    if (!window.ethereum) return alert('Please install MetaMask.');
-
+    if (!window.ethereum) return alert("Please install MetaMask.");
     const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
+      method: "eth_requestAccounts",
     });
-
     setCurrentAccount(accounts[0]);
     window.location.reload();
   };
 
   const checkIfWalletIsConnect = async () => {
-    if (!window.ethereum) return alert('Please install MetaMask.');
+    if (!window.ethereum) return alert("Please install MetaMask.");
 
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
 
     if (accounts.length) {
       setCurrentAccount(accounts[0]);
     } else {
-      console.log('No accounts found');
+      console.log("No accounts found");
     }
   };
 
-
- 
   useEffect(() => {
     checkIfWalletIsConnect();
   }, []);
+
+
   return (
-    <BlockchainConfig.Provider
-      value={{
-       
-      }}
-    >
-      {children}
-    </BlockchainConfig.Provider>
+    <BlockchainConfig.Provider value={{}}>{children}</BlockchainConfig.Provider>
   );
 };
